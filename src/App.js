60af235cec9a71b10af76f51a275125840ffcc15
DostonbekMusayev <dostonbekmusayev72@gmail.com>
2023-10-23 01:10:44 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { useState } from 'react';
 import './App.css';
-import uzb from './images/Uzbekistan.png'
+import uzbFlag from './images/Uzbekistan.png'
+import kzFlag from './images/Kazakhstan.png'
+import kgFlag from './images/Kyrgyzstan.png'
+import tjFlag from './images/Tajikistan.png'
+import tkFlag from './images/Turkmenistan.png'
 
 
 const App = () => {
   const [countries, setCountries] =useState([
   {
-     name: "O'zbekiston",
+    id:1,
+    name: "O'zbekiston",
     capital : "Toshkent",
     population: '36 millon',
     area: '448.500 km2',
     courrency: "so'm"
   },
   {
+    id:2,
     name: "Qozog'iston",
     capital : "Ostona",
     population: '18 millon',
@@ -20,6 +26,7 @@ const App = () => {
     courrency: "tenge"
   },
   {
+    id:3,
     name: "Qirg'iston",
     capital : "Bishkek",
     population: '9 millon',
@@ -27,6 +34,7 @@ const App = () => {
     courrency: "som"
   },
   {
+    id:4,
     name: 'Tojikiston',
     capital : "Dushanbe",
     population: '5 millon',
@@ -34,7 +42,8 @@ const App = () => {
     courrency: "somoni"
   },
   {
-    ame: 'Turkmaniston',
+    id:5,
+    name: 'Turkmaniston',
     capital : "Ashxobod",
     population: '10 millon',
     area: '550.200 km2',
@@ -45,16 +54,16 @@ const App = () => {
   return (
 
     <div className="App">
-      {countries.map((country)=>{
-        console.log(country.img);
+      {countries.map(country =>{
+        const {id, name, capital, population, courrency, area} = country;
         return(
-          <div className='card'>
-            <img src={uzb} alt="Country flag" width={200} height={150}/>
-            <h3>Country: {country.name}</h3>
-            <h4>Capital: {country.capital}</h4>
-            <h4> Population: {country.population}</h4>
-            <h4>Area: {country.area}</h4>
-            <h4>Courrency: {country.courrency}</h4>
+          <div key={id} className='card'>
+            <img src={id == 1 ? uzbFlag :id == 2 ? kzFlag : id == 3 ? kgFlag : id == 4 ? tjFlag : id == 5 ? tkFlag : '' } alt="Country flag" width={200} height={150}/>
+            <h3>Country: {name}</h3>
+            <h4>Capital: {capital}</h4>
+            <h4> Population: {population}</h4>
+            <h4>Area: {area}</h4>
+            <h4>Courrency: {courrency}</h4>
           </div>
         )
       })}
